a8d94ea9b61da1e54e512c2190635481799ca301
Tiago_63591 <tmgd2006@gmail.com>
2026-05-26 12:14:14 +0100

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private RigidbodyRb;
+    public float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayerRb = GetComponet<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = In
+    }
+}
