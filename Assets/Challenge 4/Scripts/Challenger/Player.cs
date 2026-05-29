32b30dbce206d3c37fa5701752144e95e8cb9c7e
a63591_TiagoDantas <tmgd2006@gmail.com>
2026-05-29 11:49:54 +0100

    challenger_futebol

---
+++ Assets/Challenge 4/Scripts/Challenger/Player.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+
+    public float speed = 10.0f;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
+
+    public float normalStrength = 10.0f;
+    public float powerupStrength = 25.0f;
+    private float powerupDuration = 5.0f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+
+        if (powerupIndicator != null)
+        {
+            powerupIndicator.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (focalPoint != null)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed, ForceMode.Force);
+        }
+    }
+
+    void Update()
+    {
+        if (powerupIndicator != null)
+        {
+            powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+
+            if (powerupIndicator != null)
+            {
+                powerupIndicator.SetActive(true);
+            }
+
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerupDuration);
+
+        hasPowerup = false;
+
+        if (powerupIndicator != null)
+        {
+            powerupIndicator.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            if (hasPowerup)
+            {
+                enemyRb.AddForce(awayFromPlayer.normalized * powerupStrength, ForceMode.Impulse);
+            }
+            else
+            {
+                enemyRb.AddForce(awayFromPlayer.normalized * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
